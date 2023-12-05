dae269f1eb717f31af1191ab0c0f8c0f2eac449c
sergio perez <spergio8@gmail.com>
2023-12-05 07:49:54 -0300

    fix ⌚

--- src/App.tsx
+++ src/App.tsx
@@ -100,12 +100,16 @@ function App() {
       {allDocs.length == 0 && <p className='p-4'>Aun no tienes reservas agendadas, crea la primera desde la parte superior ☝☝</p>  }
       {allDocs.map((elemento) => (
         <div className="card" key={elemento.id}>
-          <h5 className="card-header">{elemento.fecha}</h5>
+          <h5 className="card-header">
+            {elemento.birthdate && `🎂`} {elemento.frecuent && `🚲`} {elemento.name} ({elemento.cantidad}) 
+            </h5>
           <div className="card-body">
             <h5 className="card-title">
-            {elemento.birthdate && `🎂`} {elemento.frecuent && `🚲`} {elemento.name} ({elemento.cantidad}) 
+              {new Date(elemento.fechaYHora).toLocaleString()}
               </h5>
-            <p className="card-text">Mesa: {elemento.mesa}</p>
+            <p className="card-text">
+            Mesa: {elemento.mesa}
+              </p>
             <p onClick={() => { deleteDocument(elemento.id, true) }} className="btn btn-primary">LLEGÓ</p>
             <p onClick={() => { deleteDocument(elemento.id, false) }} className="btn btn-danger">NO VINO</p>
           </div>
