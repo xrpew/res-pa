442a07a66c9304a811bfce1c89d3d8eb44d2d4dd
sergio perez <spergio8@gmail.com>
2023-12-05 22:17:56 -0300

    coment logout for a wile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = ({handleCreateDocument}) => {
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      {/* <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Sesion
@@ -18,7 +18,7 @@ const Navbar = ({handleCreateDocument}) => {
             <li><a className="dropdown-item" href="#">cerrar sesion</a></li>
           </ul>
         </li>
-      </ul>
+      </ul> */}
      <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">nueva reserva</button>
      <div className="modal fade" id="exampleModal" tabIndex={1} aria-labelledby="exampleModalLabel" aria-hidden="true">
         <LoginContent handlePassingDataOnIndex={handleCreateDocument}/>
